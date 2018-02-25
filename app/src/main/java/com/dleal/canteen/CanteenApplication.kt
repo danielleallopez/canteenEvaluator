c52b37df28c8a0ce599863337530a64992ffee82
Daniel Leal <danielleallopez@gmail.com>
2018-02-25 21:33:50 +0100

    [Fabric]: Add Fabric support

--- app/src/main/java/com/dleal/canteen/CanteenApplication.kt
+++ app/src/main/java/com/dleal/canteen/CanteenApplication.kt
@@ -2,11 +2,14 @@ package com.dleal.canteen
 
 import android.app.Activity
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.dleal.canteen.di.AppComponent
 import com.dleal.canteen.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import io.fabric.sdk.android.Fabric
 import javax.inject.Inject
 
 /**
@@ -28,6 +31,19 @@ class CanteenApplication : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        initFabric()
+
         canteenComponent.inject(this)
+    }
+
+    private fun initFabric(){
+        val crashlyticsCore = CrashlyticsCore.Builder()
+                .disabled(false)
+                .build()
+        val crashlytics = Crashlytics.Builder()
+                .core(crashlyticsCore)
+                .build()
+        Fabric.with(applicationContext, crashlytics)
     }
 }
