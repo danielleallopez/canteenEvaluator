1749ae0a2b44d5b464597ab5e42d6cbf885cc81d
Daniel Leal <danielleallopez@gmail.com>
2019-06-15 14:25:42 +0200

    Created build.gradle.kts for dependencies management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,12 @@
+object Sdk {
+    val minVersion = 16
+    val targetVersion = 28
+    val compileVersion = 28
+}
+
+//
+//classpath 'com.android.tools.build:gradle:3.4.1'
+//classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31"
+//classpath 'io.fabric.tools:gradle:1.29.0'
+//
+//classpath 'com.google.gms:google-services:4.2.0'
